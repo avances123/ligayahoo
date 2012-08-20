301259a11f4efacfdde72461ceff4061cf6c3b56
Fabio Rueda <avances123@gmail.com>
2012-08-20 22:56:37 +0200

    Antes de meter el numpy

--- crea_equipos.py
+++ crea_equipos.py
@@ -1,49 +1,42 @@
-from scrapy.spider import BaseSpider
-from scrapy.selector import HtmlXPathSelector
-from scrapy.http import Request
-from scrapy import log
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/topics/item-pipeline.html
 
-from superliga_yahoo.items import SuperligaYahooItem
+import psycopg2
+from numpy import random
 
-class SuperligaYahooSpider(BaseSpider):
-	name = 'yahoo'
-	domain_name = "es.laliga.fantasysports.yahoo.com"
-	#start_urls = ["http://es.laliga.fantasysports.yahoo.com/football/players/00000","http://es.laliga.fantasysports.yahoo.com/football/players/19054"]
-	def start_requests(self):
-		for i in range(0,100000):
-		#for i in range(99126,99128):
-			yield Request('http://es.laliga.fantasysports.yahoo.com/football/players/%05d' % i, self.parse,meta={'id':i})
-	
+def sacaFutbolista(posicion,cantidad):
+	cur.execute("SELECT name,market_price FROM jugadores where position=%s order by RANDOM() limit %s;",(posicion,cantidad))
+	futbolistas = cur.fetchall()
+	return futbolistas
 
-	def parse(self, response):
-		hxs = HtmlXPathSelector(response)
-		#self.log('A response from %s just arrived!' % response.url)
-		# Si no existe el futbolista salimos
-		error = hxs.select('//*[@id="yspmain"]/div/div[2]/font/b').extract()
-		if len(error) > 0:
-			return None
 
-		item = SuperligaYahooItem()
-		item['id'] = response.request.meta['id']
-		item['name'] = hxs.select('//*[@id="yspmain"]/div/div/div/div[1]/div[2]/h2/text()').extract()[0].strip()
-		item['games_played'] = hxs.select('//*[@id="yspmain"]/div/div/div/div[2]/table[1]/tbody/tr[1]/td/span/text()').extract()[0].strip()
-		item['total_points'] = hxs.select('//*[@id="yspmain"]/div/div/div/div[2]/table[1]/tbody/tr[2]/td[1]/span[1]/text()').extract()[0].strip()
-		item['market_price'] = hxs.select('//*[@id="yspmain"]/div/div/div/div[2]/table[1]/tbody/tr[2]/td[2]/text()').extract()[0].strip()
-		item['price_per_point'] = hxs.select('//*[@id="yspmain"]/div/div/div/div[2]/table[3]/tbody/tr[2]/td[2]/text()').extract()[0].strip()
-		item['point_average_last3'] = hxs.select('//*[@id="yspmain"]/div/div/div/div[2]/table[3]/tbody/tr[3]/td/span/text()').extract()[0].strip()
-		item['point_average_total'] = hxs.select('//*[@id="yspmain"]/div/div/div/div[2]/table[3]/tbody/tr[4]/td[2]/text()').extract()[0].strip()
-		item['lastweek_points'] = hxs.select('//*[@id="yspmain"]/div/div/div/div[2]/table[3]/tbody/tr[4]/td[1]/text()').extract()[0].strip()
-		#item['position'] = 
-		equipo_posicion = hxs.select('//*[@id="yspmain"]/div/div/div/div[1]/div[2]/h4/text()').extract()[0].strip()
-		equipo,posicion = equipo_posicion.split(',')
-		equipo = equipo.strip()
-		posicion = posicion.strip()
-		item['team'] = equipo
-		item['position'] = posicion
+def dameEquipo(porteros,defensas,centros,delanteros):
+	lista = []
+	map(lista.append, sacaFutbolista('Portero',1))
+	map(lista.append, sacaFutbolista('Defensa',4))
+	map(lista.append, sacaFutbolista('Centrocampista',3))
+	map(lista.append, sacaFutbolista('Delantero',3))
 
-		return item
-		
+	precio = 0
+	for jugador in lista:
+		precio = precio + jugador[1]
+	return lista,precio
 
-SPIDER = SuperligaYahooSpider()
 
+if __name__ == "__main__":
+	conn = psycopg2.connect("dbname=ligayahoo user=fabio")
+	cur = conn.cursor()
+	superior = 0
+	while True:
+		equipo,precio = dameEquipo(1,4,3,3)
+		if precio > superior:
+			superior = precio
+			print
+			for jugador in equipo:
+				print jugador
+			print precio
+	cur.close()
+	conn.close()
 
