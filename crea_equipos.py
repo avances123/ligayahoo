bbd2becf59b7b69b7cf23bd064d156d1abd05e0b
Fabio Rueda <avances123@gmail.com>
2012-08-22 20:01:58 +0200

    antes de quitar el random del select

--- crea_equipos.py
+++ crea_equipos.py
@@ -3,39 +3,53 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/topics/item-pipeline.html
 
-import psycopg2
+import psycopg2,random
 
 def sacaFutbolista(posicion,cantidad):
-	cur.execute("SELECT name,market_price FROM jugadores where position=%s order by RANDOM() limit %s;",(posicion,cantidad))
+	cur = conn.cursor()
+	cur.execute("SELECT name,market_price,point_average_total FROM jugadores where position=%s and games_played >= 1 and valid = true order by RANDOM() limit %s;",(posicion,cantidad))
 	futbolistas = cur.fetchall()
+	cur.close()
 	return futbolistas
 
 
 def dameEquipo(porteros,defensas,centros,delanteros):
 	lista = []
-	map(lista.append, sacaFutbolista('Portero',1))
-	map(lista.append, sacaFutbolista('Defensa',4))
-	map(lista.append, sacaFutbolista('Centrocampista',3))
-	map(lista.append, sacaFutbolista('Delantero',3))
+	map(lista.append, sacaFutbolista('Portero',porteros))
+	map(lista.append, sacaFutbolista('Defensa',defensas))
+	map(lista.append, sacaFutbolista('Centrocampista',centros))
+	map(lista.append, sacaFutbolista('Delantero',delanteros))
 
 	precio = 0
+	puntuacion = 0
 	for jugador in lista:
 		precio = precio + jugador[1]
-	return lista,precio
+		puntuacion = puntuacion + jugador[2]
+	return lista,precio,puntuacion
 
 
 if __name__ == "__main__":
 	conn = psycopg2.connect("dbname=ligayahoo user=fabio")
-	cur = conn.cursor()
 	superior = 0
+	lista = ['433','442','352','343']
 	while True:
-		equipo,precio = dameEquipo(1,4,3,3)
-		if precio > superior:
-			superior = precio
-			print
-			for jugador in equipo:
-				print jugador
-			print precio
-	cur.close()
+		sistema = random.choice(lista)
+		if sistema == '433':
+			equipo,precio,puntuacion = dameEquipo(1,4,3,3)
+		if sistema == '442':
+			equipo,precio,puntuacion = dameEquipo(1,4,4,2)
+		if sistema == '352':
+			equipo,precio,puntuacion = dameEquipo(1,3,5,2)
+		if sistema == '343':
+			equipo,precio,puntuacion = dameEquipo(1,3,4,3)
+
+		if puntuacion > superior:
+			superior = puntuacion
+			if precio <= 100:
+				print
+				print sistema
+				for jugador in equipo:
+					print jugador
+				print precio,puntuacion
 	conn.close()
 
--- pipelines.py
+++ pipelines.py
@@ -6,17 +6,19 @@
 import psycopg2
 
 class SuperligaYahooPipeline(object):
-
+	
 	def open_spider(self,spider):
 		self.conn = psycopg2.connect("dbname=ligayahoo user=fabio")
-
+		self.cur  = self.conn.cursor()
 
 	def close_spider(self,spider):
+		self.cur.close()
 		self.conn.close()
 
 	def process_item(self, item, spider):
-		cur = self.conn.cursor()
-		cur.execute("INSERT INTO jugadores (id,name,games_played,lastweek_points,point_average_total,point_average_last3,market_price,total_points,price_per_point,team,position,valid) VALUES (%s, %s,%s, %s,%s, %s,%s, %s,%s, %s,%s, %s)",(item['id'],item['name'],item['games_played'],item['lastweek_points'],item['point_average_total'],item['point_average_last3'],item['market_price'],item['total_points'],item['price_per_point'],item['team'],item['position'],True))
-		self.conn.commit()
-		cur.close()
+		try:
+			self.cur.execute("INSERT INTO jugadores (id,name,games_played,lastweek_points,point_average_total,point_average_last3,market_price,total_points,price_per_point,team,position,valid) VALUES (%s, %s,%s, %s,%s, %s,%s, %s,%s, %s,%s, %s)",(item['id'],item['name'],item['games_played'],item['lastweek_points'],item['point_average_total'],item['point_average_last3'],item['market_price'],item['total_points'],item['price_per_point'],item['team'],item['position'],True))
+			self.conn.commit()
+		except psycopg2.IntegrityError:
+			self.conn.rollback()
 		return item
