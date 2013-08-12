fbc3c5c0589e30602c49fa0570cff52a91353ef8
Fabio Rueda <avances123@gmail.com>
2013-08-12 17:24:09 +0200

    Nueva version del bot , falta parsear cada jugador

--- settings.py
+++ settings.py
@@ -7,12 +7,12 @@
 #
 
 BOT_NAME = 'superliga_yahoo'
-BOT_VERSION = '1.0'
 
 SPIDER_MODULES = ['superliga_yahoo.spiders']
 NEWSPIDER_MODULE = 'superliga_yahoo.spiders'
 DEFAULT_ITEM_CLASS = 'superliga_yahoo.items.SuperligaYahooItem'
-USER_AGENT = '%s/%s' % (BOT_NAME, BOT_VERSION)
-ITEM_PIPELINES = [
-		    'superliga_yahoo.pipelines.SuperligaYahooPipeline',
-			]
+#USER_AGENT = '%s/%s' % (BOT_NAME, BOT_VERSION)
+#ITEM_PIPELINES = ['superliga_yahoo.pipelines.SuperligaYahooPipeline']
+#REDIRECT_ENABLED = False
+DOWNLOAD_DELAY = 2
+
--- spiders/yahoo.py
+++ spiders/yahoo.py
@@ -1,54 +1,50 @@
 from scrapy.spider import BaseSpider
+from scrapy.contrib.spiders.init import InitSpider
+
 from scrapy.selector import HtmlXPathSelector
-from scrapy.http import Request
+from scrapy.http import Request,FormRequest
 from scrapy import log
+from scrapy.contrib.spiders import Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+import keyring
 
 from superliga_yahoo.items import SuperligaYahooItem
 
-class SuperligaYahooSpider(BaseSpider):
+class SuperligaYahooSpider(InitSpider):
 	name = 'yahoo'
-	DOWNLOAD_DELAY = 2
-
-	domain_name = "es.laliga.fantasysports.yahoo.com"
-	#start_urls = ["http://es.laliga.fantasysports.yahoo.com/football/players/00000","http://es.laliga.fantasysports.yahoo.com/football/players/19054"]
-
-	def start_requests(self):
-		with open('/home/fabio/src/superliga_yahoo/superliga_yahoo/id.txt') as f:
-			for i in f.readlines():
-				j = int(i)
-				yield Request('http://es.laliga.fantasysports.yahoo.com/football/players/%d' % j, self.parse,meta={'id':j})
-
-		#for i in range(0,69854):
-		#	yield Request('http://es.laliga.fantasysports.yahoo.com/football/players/%d' % i, self.parse,meta={'id':i})
+    user = 'avances123'
+	domain_name = "es.eurosport.yahoo.com"
+	login_page = 'https://login.yahoo.com/config/login'
+	#start_urls = ['http://es.eurosport.yahoo.com/fantasy/la-liga/top']
+	#rules = (Rule(SgmlLinkExtractor(allow=r''),callback='parse_item', follow=True),)
 	
+	def init_request(self):
+	    return Request(url=self.login_page, callback=self.login)
+	
+	def login(self, response):
+	    passwd = keyring.get_password('login.yahoo.com', user)
+	    return FormRequest.from_response(response,formdata={'login': user, 'passwd': passwd },callback=self.check_login_response)
+	
+	def check_login_response(self, response):
+	    if "fabio_rueda_carrascosa" in response.body:
+	        self.log("Successfully logged in. Let's start crawling!")
+	        #return self.initialized()
+	        return self.starts_requests()
+	    else:
+	        self.log("Bad times :(")
+	
+	def starts_requests(self):
+	    #for i in range(0,69854):
+	    for i in range(38400,38402):
+	        yield Request('http://es.eurosport.yahoo.com/fantasy/la-liga/details/%d' % i, self.parse,meta={'id':i})
+    
+
 
 	def parse(self, response):
 		hxs = HtmlXPathSelector(response)
-		#self.log('A response from %s just arrived!' % response.url)
-		# Si no existe el futbolista salimos
-		error = hxs.select('//*[@id="yspmain"]/div/div[2]/font/b').extract()
-		if len(error) > 0:
-			return None
+		self.log('A response from %s just arrived!' % response.url)
 
 		item = SuperligaYahooItem()
-		item['id'] = response.request.meta['id']
-		item['name'] = hxs.select('//*[@id="yspmain"]/div/div/div/div[1]/div[2]/h2/text()').extract()[0].strip()
-		item['games_played'] = hxs.select('//*[@id="yspmain"]/div/div/div/div[2]/table[1]/tbody/tr[1]/td/span/text()').extract()[0].strip()
-		item['total_points'] = hxs.select('//*[@id="yspmain"]/div/div/div/div[2]/table[1]/tbody/tr[2]/td[1]/span[1]/text()').extract()[0].strip()
-		item['market_price'] = hxs.select('//*[@id="yspmain"]/div/div/div/div[2]/table[1]/tbody/tr[2]/td[2]/text()').extract()[0].strip()
-		item['price_per_point'] = hxs.select('//*[@id="yspmain"]/div/div/div/div[2]/table[3]/tbody/tr[2]/td[2]/text()').extract()[0].strip()
-		item['point_average_last3'] = hxs.select('//*[@id="yspmain"]/div/div/div/div[2]/table[3]/tbody/tr[3]/td/span/text()').extract()[0].strip()
-		item['point_average_total'] = hxs.select('//*[@id="yspmain"]/div/div/div/div[2]/table[3]/tbody/tr[4]/td[2]/text()').extract()[0].strip()
-		#item['lastweek_points'] = hxs.select('//*[@id="yspmain"]/div/div/div/div[2]/table[3]/tbody/tr[4]/td[1]/text()').extract()[0].strip()
-		item['lastweek_points'] = None
-
-		equipo_posicion = hxs.select('//*[@id="yspmain"]/div/div/div/div[1]/div[2]/h4/text()').extract()[0].strip()
-		equipo,posicion = equipo_posicion.split(',')
-		equipo = equipo.strip()
-		posicion = posicion.strip()
-		item['team'] = equipo
-		item['position'] = posicion
-
 		return item
 		
 
