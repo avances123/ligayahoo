c0a4cb2e8c5eb41f1bf251397db14472d71fba0f
Fabio Rueda <avances123@gmail.com>
2013-11-26 22:38:07 +0100

    funcionando

--- insert-cube.py
+++ insert-cube.py
@@ -1,5 +1,8 @@
-from pymongo import MongoClient
-
+from pymongo import MongoClient,DESCENDING,ASCENDING
+from cube import Cube, ONE_HOUR
+from datetime import datetime
+import re
+pattern = re.compile('[\W_]+')
 
 client = MongoClient()
 client = MongoClient('localhost', 27017)
@@ -7,11 +10,20 @@ db = client.ligayahoo
 jugadores = db.jugadores
 
 
-entradas = jugadores.find({},{"g1.data.weeks":{"$slice":-1},"g1.data.weeks.salary":1,"_id":0,"date":1,"g3.data.name":1})
+cube = Cube()
+
+x = 0
+entradas = jugadores.find({},{"g1.data.weeks":{"$slice":-1},"g1.data.weeks.salary":1,"date":1,"_id":0,"g3.data.name":1,"g3.data.id":1}).sort([("date",DESCENDING),]).limit(589)
 for i in entradas:
-    print i
+    #import ipdb
+    #ipdb.set_trace()
+    x = x + 1
+    player_name = i['g3']['data']['name']
+    player_name = player_name.lower()
+    player_name = pattern.sub('_', player_name)
+    salario = i['g1']['data']['weeks'][0]['salary']
+    #if salario > 15:
+    #    salario = salario + 6
+    cube.put('id_' + i['g3']['data']['id'],{'salary': salario})
 
-#import ipdb
-#ipdb.set_trace()
-
-
+print "[%s] Num eventos %d" % (datetime.now(),x)
